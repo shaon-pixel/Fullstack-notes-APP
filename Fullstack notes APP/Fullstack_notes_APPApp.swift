e10a5a93d422b30d5309b7578fc78295af2c8af3
shahriarRahmanShaon <shaon463@gmail.com>
2020-09-15 15:17:51 +0600

    initial build files added

---
+++ Fullstack notes APP/Fullstack_notes_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fullstack_notes_APPApp.swift
+//  Fullstack notes APP
+//
+//  Created by sergio shaon on 15/9/20.
+//
+
+import SwiftUI
+
+@main
+struct Fullstack_notes_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
